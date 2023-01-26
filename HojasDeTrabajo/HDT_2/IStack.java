6c832e83578394a0c159079840e1c2093ba2385e
Nelson <gar22434@uvg.edu.gt>
2023-01-25 20:23:12 -0600

    commit

--- HojasDeTrabajo/HDT_2/IStack.java
+++ HojasDeTrabajo/HDT_2/IStack.java
@@ -4,18 +4,45 @@
 package edu.uvg.structures;
 
 /**
- * @author MAAG
+ * The interface Stack.
  *
+ * @param <T> the type parameter
+ * @author MAAG
  */
 public interface IStack<T> {
 
+	/**
+	 * Count int.
+	 *
+	 * @return the int
+	 */
 	int count();
-	
+
+	/**
+	 * Is empty boolean.
+	 *
+	 * @return the boolean
+	 */
 	boolean isEmpty();
-	
+
+	/**
+	 * Push.
+	 *
+	 * @param value the value
+	 */
 	void push(T value);
-	
+
+	/**
+	 * Pull t.
+	 *
+	 * @return the t
+	 */
 	T pull();
-	
+
+	/**
+	 * Peek t.
+	 *
+	 * @return the t
+	 */
 	T peek();
 }

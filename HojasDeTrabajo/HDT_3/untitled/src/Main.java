7ff7ab68fab2a0f195132359a741a003a3e253ff
Japo <pue22296@uvg.edu.gt>
2023-02-04 20:26:05 -0600

    Agregados los métodos para gnome sort y merge sort

--- HojasDeTrabajo/HDT_3/untitled/src/Main.java
+++ HojasDeTrabajo/HDT_3/untitled/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Make an array with unordered numbers from 1 to 10
+        Integer[] myArray = new Integer[10];
+        myArray = new Integer[]{1, 3, 7, 10, 9, 2, 6, 8, 5, 4};
+
+
+        SortingMethods mySort = new SortingMethods();
+        mySort.quickSort(myArray, 0, myArray.length - 1);
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        mySort.mergeSortRecursive(myArray, 0, myArray.length - 1);
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+
+        mySort.gnomeSort(myArray);
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+
+
+
     }
 }
--- HojasDeTrabajo/HDT_3/untitled/src/SortingMethods.java
+++ HojasDeTrabajo/HDT_3/untitled/src/SortingMethods.java
@@ -1,12 +1,8 @@
+
+
 public class SortingMethods <T>{
 
-    public IComparator myCompare;
-
-    public SortingMethods(IComparator _Compare) {
-        myCompare = _Compare;
-    }
-
-    public void quickSort(T[] myArray, int inf, int sup) {
+    public <T extends Comparable<T>> void quickSort(T[] myArray, int inf, int sup) {
         int i = inf - 1;
         int j = sup;
         boolean flag = true;
@@ -21,8 +17,8 @@ public class SortingMethods <T>{
 
 
         while (flag) {
-            while(myCompare.Compare(myArray[++i], elem_div) < 0); //Move the index i until it finds an element bigger than elem_div
-            while((myCompare.Compare(myArray[--j], elem_div) > 0)  && (j > inf)); //Move the index j until it finds element smaller than elem_div
+            while((myArray[++i].compareTo(elem_div)) < 0); //Move the index i until it finds an element bigger than elem_div
+            while(((myArray[--j].compareTo(elem_div)) > 0)  && (j > inf)); //Move the index j until it finds element smaller than elem_div
 
             if (i < j) {
                 temp = myArray[i];
@@ -40,4 +36,61 @@ public class SortingMethods <T>{
         quickSort(myArray, i + 1, sup);
     }
 
+    public <T extends Comparable<T>> T[] gnomeSort(T[] arr) {
+        int index = 0;
+        while (index < arr.length) {
+            if (index == 0 || arr[index].compareTo(arr[index - 1]) >= 0) {
+                index++;
+            } else {
+                T temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
+        return arr;
+    }
+
+    public <T extends Comparable<T>> void mergeSortRecursive(T[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSortRecursive(arr, start, mid);
+            mergeSortRecursive(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] arr, int start, int mid, int end) {
+        int n1 = mid - start + 1;
+        int n2 = end - mid;
+        T[] left = (T[]) new Comparable[n1];
+        T[] right = (T[]) new Comparable[n2];
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[start + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
+        }
+        int i = 0, j = 0, k = start;
+        while (i < n1 && j < n2) {
+            if (left[i].compareTo(right[j]) <= 0) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
 }

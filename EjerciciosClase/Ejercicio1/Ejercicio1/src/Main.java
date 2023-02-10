813ab84991536fdf481bd64c98d042ac9c0f3006
Nelson <gar22434@uvg.edu.gt>
2023-02-10 12:21:39 -0600

    inicio de sesion hecho

--- EjerciciosClase/Ejercicio1/Ejercicio1/src/Main.java
+++ EjerciciosClase/Ejercicio1/Ejercicio1/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        User user;
+        UserInstanceCreator creador = new UserInstanceCreator();
+
+        Scanner miScanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre de usuario");
+        String username = miScanner.nextLine();
+        System.out.println("Ingrese su contraseña");
+        String password = miScanner.nextLine();
+
+        if(username.equals("listener") && password.equals("listener")) {
+            //user = creador.createUser("listener");
+        } else if(username.equals("producer") && password.equals("producer")) {
+            //user = creador.createUser("producer");
+        } else {
+            System.out.println("Usuario o contraseña incorrectos");
+            return;
+        }
+
+
+
+
     }
 }

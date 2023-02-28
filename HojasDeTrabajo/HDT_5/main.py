5c1c880b98be31a655f4033a77d8d09d8baf002a
Nelson <gar22434@uvg.edu.gt>
2023-02-27 18:51:13 -0600

    creacion de una funcion que calcule el tiempo promedio de cada proceso

--- HojasDeTrabajo/HDT_5/main.py
+++ HojasDeTrabajo/HDT_5/main.py
@@ -8,6 +8,7 @@
 from simpy import Environment, Resource, Container
 from random import seed, expovariate, randint
 from matplotlib.pyplot import *
+import numpy as np
 
 
 NUCLEOS = 1
@@ -18,11 +19,13 @@ NOPROCESOS = 25
 INTERVALO = 10
 CICLOS = 2
 
+
 #Colas
 ioWait = []
 ready = []
 
 class Process:
+    
     def __init__(self, env, name, ramUse):
         self.env = env
         self.name = name
@@ -30,12 +33,16 @@ class Process:
         self.totalInst = randint(1,10)
         self.leftInst = self.totalInst
         self.cpu_time = 0
-
+        self.tiempo_inicio = 0
+        self.tiempo_final = 0
+        
     def run(self, cpu):
         instructions = min(3, self.leftInst)
         self.cpu_time = instructions * CICLOS
         self.leftInst -= instructions
+        self.tiempo_inicio = self.env.now
         yield self.env.timeout(self.cpu_time)
+        self.tiempo_final = self.env.now
         if self.leftInst <= 0:
             print(f'Proceso No. {self.name} terminado en t={round(self.env.now)}')
             yield ram.put(self.ramUse)
@@ -72,12 +79,27 @@ def cpuScheduler(env, cpu):
     while True:
         if len(ready) > 0:
             process = ready.pop(0)
-
+            tiempos_procesos = {}
             with cpu.request() as req:
                 print(f'Proceso No. {process.name} corriendo en {round(env.now)}')
                 yield env.process(process.run(cpu))
-
+                tiempos_procesos[process.name] = (process.tiempo_inicio, process.tiempo_final)
         yield env.timeout(CICLOS)
+
+def calcTiempos(env):
+        times = []
+        for name, (start_time, end_time) in tiempos_procesos.items():
+            time_in_system = end_time - start_time
+            times.append(time_in_system)
+
+        mean_time = np.mean(times)
+        std_dev = np.std(times)
+
+        print(f"Average time in system: {mean_time}")
+        print(f"Standard deviation: {std_dev}")
+
+
+       
 
 env = Environment()
 ram = Container(env, init=CAPACIDADRAM, capacity=CAPACIDADRAM)
@@ -86,6 +108,18 @@ cpu = Resource(env, capacity=NUCLEOS)
 env.process(genProcess(env, ram))
 env.process(cpuScheduler(env,cpu))
 env.process(ioScheduler(env))
+env.process(calcTiempos())
 
 env.run(until=450)
 
+""""
+
+#Graficas
+data = [3, 4, 5, 6, 7]
+labels = ['A', 'B', 'C', 'D', 'E']
+std_dev = np.std(data)
+mean = np.mean(data)
+
+bar(labels, data)
+show()
+"""

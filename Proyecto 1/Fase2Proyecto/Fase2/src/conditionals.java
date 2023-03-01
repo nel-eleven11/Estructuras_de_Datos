273e14482e8cd838c7b6d4839b2e01d2981c1de0
Japo <pue22296@uvg.edu.gt>
2023-02-28 18:18:40 -0600

    Lógica de la clase conditionals agregada

--- Proyecto 1/Fase2Proyecto/Fase2/src/conditionals.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/conditionals.java
@@ -1,2 +1,37 @@
 public class conditionals {
+
+    public static boolean conditionalCheck(int a, int b, String symbol){
+        switch (symbol) {
+            case ">":
+                if (a > b) {
+                    return true;
+                }
+            case "<":
+                if (a < b) {
+                    return true;
+                }
+            case ">=":
+                if (a >= b) {
+                    return true;
+                }
+            case "<=":
+                if (a <= b) {
+                    return true;
+                }
+            case "==":
+                if (a == b) {
+                    return true;
+                }
+                break;
+            case "!=":
+                if (a != b) {
+                    return true;
+                }
+                break;
+            default:
+                return false;
+        }
+        return false;
+    }
+
 }

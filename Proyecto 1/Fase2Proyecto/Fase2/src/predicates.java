3e01075fb9d0c99edf63dac764f7111e9f63db72
Nelson <gar22434@uvg.edu.gt>
2023-03-02 17:52:37 -0600

    clase de predicados hecha, predicados básicos

--- Proyecto 1/Fase2Proyecto/Fase2/src/predicates.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/predicates.java
@@ -5,5 +5,41 @@ Oscar Fuentes
 Proyecto 1 Algoritmos y Estructuras de datos
  */
 
+
+
+import org.apache.commons.lang3.StringUtils;
+
 public class predicates {
+
+    public static boolean verificar(String simbolo, String a, String b){
+        boolean resultado = false;
+
+        switch (simbolo){
+            case "numberp":
+                resultado = StringUtils.isNumeric(a);
+                break;
+            case "symbolp":
+                resultado = StringUtils.isAlpha(a);
+                break;
+            case "zerop":
+                resultado = a.equals("0");
+                break;
+            case "oddp":
+                resultado = Integer.parseInt(a) % 2 != 0;
+                break;
+            case "evenp":
+                resultado = Integer.parseInt(a) % 2 == 0;
+                break;
+            case "atom":
+                resultado = StringUtils.isAlpha(a) || StringUtils.isNumeric(a);
+                break;
+            case "eq":
+                resultado = a.equals(b);
+                break;
+            default:
+                break;
+        }
+        return resultado;
+    }
+
 }

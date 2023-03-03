d9e1b0f32b476fe3b0c37946a23d6793b436f547
Nelson <gar22434@uvg.edu.gt>
2023-03-03 11:17:55 -0600

    clase main creada

---
+++ HojasDeTrabajo/HDT_6/src/main.java
@@ -0,0 +1,13 @@
+
+/*
+Nelson García 22434
+Joaquín Puente 22
+Oscar Fuentes 22
+ */
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

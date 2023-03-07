fa35599de36dadeb5f5bbe694adbe98c14cd25e3
Nelson <gar22434@uvg.edu.gt>
2023-03-06 22:21:38 -0600

    commit

--- HojasDeTrabajo/HDT_6/src/Controlador.java
+++ HojasDeTrabajo/HDT_6/src/Controlador.java
@@ -5,15 +5,21 @@ HDT 6
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Map;
 public class Controlador {
 
+    private File archivo;
     private ArrayList<String> items = new ArrayList<String>();
     Factory_Map factory = new Factory_Map();
     ArrayList<String> producto2 = new ArrayList<String>();
     ArrayList<String> categoria = new ArrayList<String>();
+    ArrayList<Integer> totalPro = new ArrayList<Integer>();
+
+    Map<String, Map<String, Integer>> map;
 
     public void importFile(String nombreDelArchivo){
         //Ruta del archivo
@@ -33,14 +39,27 @@ public class Controlador {
         }
     }
 
+    public void writFile(String linea,String nombreDelArchivo){
+        archivo = new File(nombreDelArchivo);
+        try {
+            FileWriter miEscritor = new FileWriter(archivo);
+            miEscritor.write(linea);
+            miEscritor.close();
+        } catch (IOException e) {
+            // Ocurrió un error en la escritura
+            e.printStackTrace();
+        }
+
+    }
+
     public ArrayList<String> getItems() {
         return items;
     }
 
-    public void cargarProductos(ArrayList<String> productos, int tipoDeMapa) {
+    public Map<String, Map<String, Integer>> cargarProductos(ArrayList<String> productos, int tipoDeMapa) {
 
 
-        Map<String, Map<String, Integer>> map = factory.getMap(tipoDeMapa);
+        map = factory.getMap(tipoDeMapa);
 
         Map<String, Integer> desproducto = factory.getMap(tipoDeMapa);
 
@@ -58,22 +77,42 @@ public class Controlador {
             map.put(categoria.get(i), desproducto);
 
         }
-
+        /*
         for (int i = 0; i < productos.size(); i++) {
             System.out.println(categoria.get(i) +" "+producto2.get(i)+" "+map.get(categoria.get(i)).get(producto2.get(i)));
+        }*/
+        return map;
+
+    }
+
+    public void agregarProducto(int categoria_, int producto_, int cantidad, int tipoDeMapa){
+
+        map = cargarProductos(items, tipoDeMapa);
+        String categoria2 = categoria.get(categoria_);
+        String producto = producto2.get(producto_);
+        map.get(categoria2).put(producto, cantidad);
+        System.out.println("Agregado: "+categoria.get(categoria_)+producto2.get(producto_)+map.get(categoria2).get(producto));
+        //escribir en el archivo
+        String linea = categoria.get(categoria_)+" | "+producto2.get(producto_);
+        writFile(linea, "ListadoProducto (2)");
+    }
+    public void mostrarProducto(String nomProducto, int tipoDeMapa){
+        map = factory.getMap(tipoDeMapa);
+        map = cargarProductos(items, tipoDeMapa);
+        //System.out.println("Producto: "+map.get(categoria_)+" "+map.get(categoria_).get(producto2.get(categoria_)));
+
+
+    }
+
+    public void mostrarCategoria(String nomProducto, int tipoDeMapa){
+        map = factory.getMap(tipoDeMapa);
+        map = cargarProductos(items, tipoDeMapa);
+        for(int i = 0; i < producto2.size(); i++){
+            if(producto2.get(i).equals(nomProducto)){
+                System.out.println("Categoría: "+categoria.get(i));
+            }
         }
 
-    }
-
-    public void agregarProducto(int categoria, int producto, int cantidad){
-
-    }
-
-    public void mostrarProductos(int categoria){
-
-    }
-
-    public void mostrarCategorias(int nomProducto){
 
     }
 
--- HojasDeTrabajo/HDT_6/src/main.java
+++ HojasDeTrabajo/HDT_6/src/main.java
@@ -17,7 +17,7 @@ public class main {
 
         int opcion = 0;
 
-        JOptionPane.showMessageDialog(null, "Bienvenido a la tienda onnline");
+        JOptionPane.showMessageDialog(null, "Bienvenido a la tienda online");
 
 
         while(opcion != 7){
@@ -37,37 +37,37 @@ public class main {
                         case 1:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\n1. Mesas de jardín\n2. Sillas de jardín\n3. Conjuntos mesas y sillas de jardín\n4. Mesas de Ping Pong exteriores"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         case 2:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\n1. Cojines y colchonetas de masaje\n2. Sillones relax y sofás de masajes\n3. Sillones de masaje avanzados\n4. Sofás camas"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         case 3:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\1. Cerveza tibetana Barley\n2. Té frio\n3. Coca cola 1 litro\n4. Coca cola 2 litros"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         case 4:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\n1. Sirope de regaliz\n2. Especies Cajun del chef\n3. Mezcla Gumbo del chef"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         case 5:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\n1. Peras secas\n2. Pasas\n3. Manzanas rojas\n4. Manzanas verdes"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         case 6:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\n1. Res\n2. Pollo\n3. Cerdo\n4. Camarones\n5. Pescado"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         case 7:
                             producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el producto\n1. Queso de cabra\n2. Queso Manchego\n3.Leche descremada\n4. Leche deslactosada\n5. Leche entera"));
                             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad que desea agregar"));
-                            controlador.agregarProducto(categoria, producto, cantidad);
+                            controlador.agregarProducto(categoria-1, producto-1, cantidad, mapa);
                             break;
                         default:
                             JOptionPane.showMessageDialog(null, "Categoría no válida");
@@ -77,9 +77,13 @@ public class main {
                     break;
                 case 2:
                     JOptionPane.showMessageDialog(null, "Mostrar la categoría del producto");
+                    String nombre = JOptionPane.showInputDialog("Ingrese el nombre del producto");
+                    controlador.mostrarCategoria(nombre, mapa);
                     break;
                 case 3:
                     JOptionPane.showMessageDialog(null, "Mostrar los datos del producto");
+                    nombre = JOptionPane.showInputDialog("Ingrese el nombre del producto");
+                    controlador.mostrarProducto(nombre, mapa);
                     break;
                 case 4:
                     JOptionPane.showMessageDialog(null, "Mostrar productos ordenados");

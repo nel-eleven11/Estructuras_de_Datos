3df27be010a70e7256a0b3ade6b928deeab004a4
Japo <pue22296@uvg.edu.gt>
2023-03-10 11:48:49 -0600

    Funcionalidad de variables y constantes agregado

--- Proyecto 1/Fase2Proyecto/Fase2/src/variables.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/variables.java
@@ -4,11 +4,28 @@ Joaquín Puente 22296
 Oscar Fuentes
 Proyecto 1 Algoritmos y Estructuras de datos
  */
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class  variables <T>{
+public class  variables{
+    private HashMap<String, Object> variables = new HashMap<String, Object>();
 
-    public <T> asignacion(String variable, <T> valor){
-
-
+    public void addVariable(String name, Object value){
+        variables.put(name, value);
     }
+
+    public Object getVariable(String name){
+        return variables.get(name);
+    }
+
+    private HashMap<String, Object> constants= new HashMap<String, Object>();
+
+    public void addConstant(String name, Object value){
+        constants.put(name, value);
+    }
+
+    public Object getConstant(String name){
+        return constants.get(name);
+    }
+
 }

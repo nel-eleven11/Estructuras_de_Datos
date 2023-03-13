0089662a6ce5e331f2b970d4103fc4b751479ec0
Japo <pue22296@uvg.edu.gt>
2023-03-12 19:26:18 -0600

    Switch enhanced para conditionals y clase params eliminada

--- Proyecto 1/Fase2Proyecto/Fase2/src/conditionals.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/conditionals.java
@@ -11,55 +11,47 @@ public class conditionals {
     public static String verificarCondicion(String cond, String [] op,String [] a, String [] b, String [] consecuencias){
         boolean respuesta = false;
         String resp = "";
-        switch (cond){
-            case "if":
+        switch (cond) {
+            case "if" -> {
                 respuesta = predicates.verificar(op[0], a[0], b[0]);
                 if (respuesta) {
-                    resp= consecuencias[0];
-                }
-                else{
-                    if(consecuencias.length > 1)
+                    resp = consecuencias[0];
+                } else {
+                    if (consecuencias.length > 1)
                         resp = consecuencias[1];
-                    else{
+                    else {
                         resp = "NIL";
                     }
                 }
-                break;
-            case "cond":
+            }
+            case "cond" -> {
                 for (int i = 0; i < op.length; i++) {
                     respuesta = predicates.verificar(op[i], a[i], b[i]);
                     if (respuesta) {
                         resp = consecuencias[i];
                         break;
-                    }
-                    else{
+                    } else {
                         resp = "NIL";
                     }
                 }
-
-                break;
-            case "when":
+            }
+            case "when" -> {
                 respuesta = predicates.verificar(op[0], a[0], b[0]);
                 if (respuesta) {
-                    resp = consecuencias[consecuencias.length-1];
-                }
-                else{
+                    resp = consecuencias[consecuencias.length - 1];
+                } else {
                     resp = "NIL";
                 }
-                break;
-            case "unless":
-                respuesta = predicates.verificar(op[0],  a[0], b[0]);
+            }
+            case "unless" -> {
+                respuesta = predicates.verificar(op[0], a[0], b[0]);
                 if (!respuesta) {
-                    resp = consecuencias[consecuencias.length-1];
-                }
-                else{
+                    resp = consecuencias[consecuencias.length - 1];
+                } else {
                     resp = "NIL";
                 }
-                break;
-            default:
-                resp = "Error";
-                break;
-
+            }
+            default -> resp = "Error";
         }
         return resp;
     }

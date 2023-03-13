d684334bb10719f676299093078e2701980a72f2
oscarfuentes22 <97373140+oscarfuentes22@users.noreply.github.com>
2023-03-12 20:46:00 -0600

    tokens de la funcion quote, print y pred

--- Proyecto 1/Fase2Proyecto/Fase2/src/tokens.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/tokens.java
@@ -1,20 +1,36 @@
+import javax.swing.*;
+
 public class tokens {
 
-    public static void setq(String variable, String value){
+    public static void setq(String variable, String value) {
         variables.addVariable(variable, value);
     }
 
-    public static void defun(String function, String value){
+    public static void defun(String function, String value) {
         functions.addFunction(function, value);
     }
 
-    public static void setConst(String constant, String value){
+    public static void setConst(String constant, String value) {
         variables.addConstant(constant, value);
     }
 
-    public static double arithmetic(double a, double b, String op){
+    public static double arithmetic(double a, double b, String op) {
         return arithmetic.verificarOp(a, b, op);
     }
 
-
+    /**
+     * quote sirve para pasar de cualquier tipo de dato a una String
+     *
+     * @param value
+     * @return String
+     */
+    public static String quote(Object value) {
+        return value.toString();
+    }
+    public  static boolean pred(String simbolo,String a, String b){
+        return predicates.verificar(simbolo,a,b);
+    }
+    public static void print(Object value) {
+        System.out.println(value);
+    }
 }

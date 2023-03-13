6d5186f740d54b977caa89e48535b8e7057697ff
Japo <pue22296@uvg.edu.gt>
2023-03-12 21:08:52 -0600

    Decodificador de los comandos lisp comenzados

--- Proyecto 1/Fase2Proyecto/Fase2/src/interpreter.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/interpreter.java
@@ -7,23 +7,30 @@ Proyecto 1 Algoritmos y Estructuras de datos
 
 public class interpreter {
 
-    public String[] parser(String input){
-        String[] tokens = input.split(" ");
-        return tokens;
-    }
+    public static void interpretar(String comando) {
+        comando = comando.substring(1, comando.length() - 1);
 
-    public void instructionReader(String[] tokens){
-        for (int i = 0; i < tokens.length; i++) {
-            System.out.println(tokens[i]);
+        String[] comandoSeparado = comando.split(" ");
+
+        if (comandoSeparado[0].equalsIgnoreCase("defun")) {
+            String nombreFuncion = comandoSeparado[1];
+            String[] parametros = comandoSeparado[2].split(",");
+            String[] cuerpo = comando.substring(comando.indexOf("(") + 1, comando.lastIndexOf(")")).split(" ");
+            tokens.defun(nombreFuncion, parametros, cuerpo);
+        } else if (comandoSeparado[0].equalsIgnoreCase("setq")) {
+            tokens.setq(comandoSeparado[1], comandoSeparado[2]);
+        } else if (comandoSeparado[0].equalsIgnoreCase("defconstant")) {
+            tokens.setConst(comandoSeparado[1], comandoSeparado[2]);
+        } else if (comandoSeparado[0].equalsIgnoreCase("+") || comandoSeparado[0].equalsIgnoreCase("-") || comandoSeparado[0].equalsIgnoreCase("*") || comandoSeparado[0].equalsIgnoreCase("/") || comandoSeparado[0].equalsIgnoreCase("mod") || comandoSeparado[0].equalsIgnoreCase("exp") || comandoSeparado[0].equalsIgnoreCase("sqrt")){
+            tokens.arithmetic(Integer.parseInt(comandoSeparado[1]), Integer.parseInt(comandoSeparado[2]), comandoSeparado[0]);
+        } else if (comandoSeparado[0].equalsIgnoreCase("if") || comandoSeparado[0].equalsIgnoreCase("cond") || comandoSeparado[0].equalsIgnoreCase("when") || comandoSeparado[0].equalsIgnoreCase("unless")) {
+
+        } else if (comandoSeparado[0].equalsIgnoreCase("quote")) {
+            tokens.quote(comandoSeparado[1]);
+        } else if () {
+            
         }
-    }
 
-    public void verifier(String[] tokens){
-        for (int i = 0; i < tokens.length; i++) {
-            if(tokens[i].equals("print")){
-                System.out.println(tokens[i+1]);
-            }
-        }
-    }
 
+    }
 }

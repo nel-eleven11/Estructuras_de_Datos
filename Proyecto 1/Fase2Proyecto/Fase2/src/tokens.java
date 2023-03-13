13bc2e8300235d4a96923f889061d3a01e31d99c
Japo <pue22296@uvg.edu.gt>
2023-03-12 18:26:43 -0600

    Token setq, defun, const y arithmetic establecidos

--- Proyecto 1/Fase2Proyecto/Fase2/src/tokens.java
+++ Proyecto 1/Fase2Proyecto/Fase2/src/tokens.java
@@ -1,3 +1,20 @@
 public class tokens {
 
+    public static void setq(String variable, String value){
+        variables.addVariable(variable, value);
+    }
+
+    public static void defun(String function, String value){
+        functions.addFunction(function, value);
+    }
+
+    public static void setConst(String constant, String value){
+        variables.addConstant(constant, value);
+    }
+
+    public static double arithmetic(double a, double b, String op){
+        return arithmetic.verificarOp(a, b, op);
+    }
+
+
 }

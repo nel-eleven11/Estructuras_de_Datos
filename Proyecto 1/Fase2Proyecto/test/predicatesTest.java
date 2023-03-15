ffe1002e9cd87bfcbc581eeed4fbf37012e636de
oscarfuentes22 <97373140+oscarfuentes22@users.noreply.github.com>
2023-03-15 13:45:00 -0600

    Test de predicates completado

--- Proyecto 1/Fase2Proyecto/test/predicatesTest.java
+++ Proyecto 1/Fase2Proyecto/test/predicatesTest.java
@@ -8,6 +8,23 @@ class predicatesTest {
     void verificar() {
 
 
-        assertEquals(true, predicates.verificar("numberp", "1", "2"));
+        assertTrue(predicates.verificar("numberp", "1", "2"));
+        assertTrue(predicates.verificar("symbolp", "a", "2"));
+        assertTrue(predicates.verificar("zerop", "0", "2"));
+        assertEquals(true,predicates.verificar("oddp", "1", "2"));
+        assertEquals(true,predicates.verificar("evenp", "2", "2"));
+        assertEquals(true,predicates.verificar("atom", "a", "2"));
+        assertEquals(true,predicates.verificar("eq", "a", "a"));
+        assertEquals(true,predicates.verificar("listp", "'(a b c)", "2"));
+        assertEquals(true,predicates.verificar(">", "2", "1"));
+        assertEquals(true,predicates.verificar("<", "1", "2"));
+        assertEquals(true,predicates.verificar(">=", "2", "1"));
+        assertEquals(true,predicates.verificar("<=", "1", "2"));
+        assertEquals(true,predicates.verificar("==", "1", "1"));
+        assertEquals(true,predicates.verificar("!=", "1", "2"));
+        assertEquals(true,predicates.verificar("not", "a"));
+        assertEquals(true,predicates.verificar("and", "T", "T"));
+        assertEquals(true,predicates.verificar("or", "T", "F"));
+
     }
 }

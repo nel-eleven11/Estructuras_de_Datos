7d10c5431a6a1502b62d245706a823e188878989
Japo <pue22296@uvg.edu.gt>
2023-03-29 22:03:14 -0600

    Orden de prioridades establecido de menor número más arriba en el árbol

--- HojasDeTrabajo/HDT_8/src/ComparadorNumeros.java
+++ HojasDeTrabajo/HDT_8/src/ComparadorNumeros.java
@@ -14,7 +14,7 @@ public class ComparadorNumeros<Integer> implements Comparator<Integer> {
     public int compare(Integer o1, Integer o2) {
         if ((int)o1 == (int)o2)
             return 0;
-        else if ((int)o1 > (int)o2)
+        else if ((int)o1 < (int)o2)
             return 1;
         else
             return -1;

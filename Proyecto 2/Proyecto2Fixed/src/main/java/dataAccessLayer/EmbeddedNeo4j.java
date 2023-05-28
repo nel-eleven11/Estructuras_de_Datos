59f96490327733cfbd24bb651e285bfb4e8f1117
Nelson <gar22434@uvg.edu.gt>
2023-05-27 18:45:37 -0600

    arreglos en el proyecto

--- Proyecto 2/Proyecto2Fixed/src/main/java/dataAccessLayer/EmbeddedNeo4j.java
+++ Proyecto 2/Proyecto2Fixed/src/main/java/dataAccessLayer/EmbeddedNeo4j.java
@@ -1,14 +1,24 @@
 /**
- *
+ * 
  */
 package dataAccessLayer;
 
-import org.neo4j.driver.*;
-
-import java.util.LinkedList;
-import java.util.List;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+import org.neo4j.driver.summary.ResultSummary;
 
 import static org.neo4j.driver.Values.parameters;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 /**
  * @author Administrator
  *
@@ -16,7 +26,7 @@ import static org.neo4j.driver.Values.parameters;
 public class EmbeddedNeo4j implements AutoCloseable{
 
     private final Driver driver;
-
+    
 
     public EmbeddedNeo4j( String uri, String user, String password )
     {
@@ -39,8 +49,8 @@ public class EmbeddedNeo4j implements AutoCloseable{
                 public String execute( Transaction tx )
                 {
                     Result result = tx.run( "CREATE (a:Greeting) " +
-                                    "SET a.message = $message " +
-                                    "RETURN a.message + ', from node ' + id(a)",
+                                                     "SET a.message = $message " +
+                                                     "RETURN a.message + ', from node ' + id(a)",
                             parameters( "message", message ) );
                     return result.single().get( 0 ).asString();
                 }
@@ -48,41 +58,41 @@ public class EmbeddedNeo4j implements AutoCloseable{
             System.out.println( greeting );
         }
     }
-
+    
     public LinkedList<String> getActors()
     {
-        try ( Session session = driver.session() )
-        {
-
-
-            LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
-            {
-                @Override
-                public LinkedList<String> execute( Transaction tx )
-                {
-                    Result result = tx.run( "MATCH (people:Person) RETURN people.name");
-                    LinkedList<String> myactors = new LinkedList<String>();
-                    List<Record> registros = result.list();
-                    for (int i = 0; i < registros.size(); i++) {
-                        //myactors.add(registros.get(i).toString());
-                        myactors.add(registros.get(i).get("people.name").asString());
-                    }
-
-                    return myactors;
-                }
-            } );
-
-            return actors;
-        }
+    	 try ( Session session = driver.session() )
+         {
+    		 
+    		 
+    		 LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
+             {
+                 @Override
+                 public LinkedList<String> execute( Transaction tx )
+                 {
+                     Result result = tx.run( "MATCH (people:Person) RETURN people.name");
+                     LinkedList<String> myactors = new LinkedList<String>();
+                     List<Record> registros = result.list();
+                     for (int i = 0; i < registros.size(); i++) {
+                    	 //myactors.add(registros.get(i).toString());
+                    	 myactors.add(registros.get(i).get("people.name").asString());
+                     }
+                     
+                     return myactors;
+                 }
+             } );
+             
+             return actors;
+         }
     }
-
+    
     public LinkedList<String> getMoviesByActor(String actor)
     {
-        try ( Session session = driver.session() )
+   	 try ( Session session = driver.session() )
         {
-
-
-            LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
+   		 
+   		 
+   		 LinkedList<String> actors = session.readTransaction( new TransactionWork<LinkedList<String>>()
             {
                 @Override
                 public LinkedList<String> execute( Transaction tx )
@@ -91,47 +101,66 @@ public class EmbeddedNeo4j implements AutoCloseable{
                     LinkedList<String> myactors = new LinkedList<String>();
                     List<Record> registros = result.list();
                     for (int i = 0; i < registros.size(); i++) {
-                        //myactors.add(registros.get(i).toString());
-                        myactors.add(registros.get(i).get("actorMovies.title").asString());
+                   	 //myactors.add(registros.get(i).toString());
+                   	 myactors.add(registros.get(i).get("actorMovies.title").asString());
                     }
-
+                    
                     return myactors;
                 }
             } );
-
+            
             return actors;
         }
-    }
-
-    public String createUser(String Name, String lastName, String age) {
-        try (Session session = driver.session()) {
-            String result = session.writeTransaction(new TransactionWork<String>() {
+   }
+    
+    public String insertMovie(String title, int releaseYear, String tagline) {
+    	try ( Session session = driver.session() )
+        {
+   		 
+   		 String result = session.writeTransaction( new TransactionWork<String>()
+   		 
+            {
                 @Override
-                public String execute(Transaction tx) {
-                    tx.run("CREATE (u:User {firstName:'" + Name + "', lastName:" + lastName + "', age:" + age + ", score: 0})")
-                    ;
+                public String execute( Transaction tx )
+                {
+                    tx.run( "CREATE (Test:Movie {title:'" + title + "', released:"+ releaseYear +", tagline:'"+ tagline +"'})");
+                    
                     return "OK";
-
-
                 }
-            });
+            }
+   		 
+   		 );
+            
             return result;
         } catch (Exception e) {
-            return e.getMessage();
+        	return e.getMessage();
         }
     }
+
+    public void createUser(String userId, int age, String name, String lastName) {
+        String query = "MERGE (u:User {userId: $userId}) " +
+                "ON CREATE SET u.name = $name, u.lastName = $lastName u.age = $age";
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("userId", userId);
+        parameters.put("name", name);
+        parameters.put("lastName", lastName);
+        parameters.put("age", age);
+
+        try (Session session = driver.session()) {
+            session.run(query, parameters);
+        }
+    }
+
 
 
     public String linkUserToCity(String userId, String cityName) {
         try (Session session = driver.session()) {
-            String result = session.writeTransaction(new TransactionWork<String>() {
-                @Override
-                public String execute(Transaction tx) {
-                    tx.run("MATCH (u:User {userId: $userId}), (l:Location {name: $cityName})" +
-                                    "MERGE (u)-[:LIVES_IN]->(l)",
-                            parameters("userId", userId, "cityName", cityName));
-                    return "OK";
-                }
+            String result = session.writeTransaction(tx -> {
+                tx.run("MATCH (u:User {userId: $userId}), (l:Location {name: $cityName})" +
+                                "MERGE (u)-[:LIVES_IN]->(l)",
+                        parameters("userId", userId, "cityName", cityName));
+                return "OK";
             });
             return result;
         } catch (Exception e) {
@@ -142,18 +171,14 @@ public class EmbeddedNeo4j implements AutoCloseable{
 
     public String linkUserToInterests(String userId, List<String> interests) {
         try (Session session = driver.session()) {
-            String result = session.writeTransaction(new TransactionWork<String>() {
-                @Override
-                public String execute(Transaction tx) {
-                    for (String interest : interests) {
-                        tx.run("MATCH (u:User {userId: $userId}), (i:Interest {name: $interest})" +
-                                        "MERGE (u)-[:INTERESTED_IN]->(i)",
-                                parameters("userId", userId, "interest", interest));
-                    }
-                    return "OK";
+            return session.writeTransaction(tx -> {
+                for (String interest : interests) {
+                    tx.run("MATCH (u:User {userId: $userId}), (i:Interest {name: $interest})" +
+                                    "MERGE (u)-[:INTERESTED_IN]->(i)",
+                            parameters("userId", userId, "interest", interest));
                 }
+                return "OK";
             });
-            return result;
         } catch (Exception e) {
             return e.getMessage();
         }
@@ -162,16 +187,12 @@ public class EmbeddedNeo4j implements AutoCloseable{
 
     public String linkUserToRelationshipType(String userId, String relationshipType) {
         try (Session session = driver.session()) {
-            String result = session.writeTransaction(new TransactionWork<String>() {
-                @Override
-                public String execute(Transaction tx) {
-                    tx.run("MATCH (u:User {userId: $userId}), (r:RelationshipType {name: $relationshipType})" +
-                                    "MERGE (u)-[:WANTS_RELATIONSHIP]->(r)",
-                            parameters("userId", userId, "relationshipType", relationshipType));
-                    return "OK";
-                }
+            return session.writeTransaction(tx -> {
+                tx.run("MATCH (u:User {userId: $userId}), (r:RelationshipType {name: $relationshipType})" +
+                                "MERGE (u)-[:WANTS_RELATIONSHIP]->(r)",
+                        parameters("userId", userId, "relationshipType", relationshipType));
+                return "OK";
             });
-            return result;
         } catch (Exception e) {
             return e.getMessage();
         }
@@ -179,16 +200,12 @@ public class EmbeddedNeo4j implements AutoCloseable{
 
     public String linkUserToSex(String userId, String sex) {
         try (Session session = driver.session()) {
-            String result = session.writeTransaction(new TransactionWork<String>() {
-                @Override
-                public String execute(Transaction tx) {
-                    tx.run("MATCH (u:User {userId: $userId}), (s:Sex {type: $sex})" +
-                                    "MERGE (u)-[:IS_OF_SEX]->(s)",
-                            parameters("userId", userId, "sex", sex));
-                    return "OK";
-                }
+            return session.writeTransaction(tx -> {
+                tx.run("MATCH (u:User {userId: $userId}), (s:Sex {type: $sex})" +
+                                "MERGE (u)-[:IS_OF_SEX]->(s)",
+                        parameters("userId", userId, "sex", sex));
+                return "OK";
             });
-            return result;
         } catch (Exception e) {
             return e.getMessage();
         }
